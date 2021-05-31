7c107d9c9de293a8bc889d7ba89519f8da5fa2f2
sebastian <sebastian.matkovich@gmail.com>
2021-05-31 13:33:57 +0200

    spelling mistake empimage instead of tempimage

--- label.sh
+++ label.sh
@@ -2,5 +2,5 @@ while true
 do
 	python3 /home/pi/scan.py
 	./maketemp.sh
-	/home/pi/crop.sh&&rm -f /home/pi/*.cr2&&sudo rsync -avz --exclude='empimage.jpg' --include='*.jpg' --exclude='*' labeled/ /usb/&&rm -rf *.jpg
+	/home/pi/crop.sh&&rm -f /home/pi/*.cr2&&sudo rsync -avz --exclude='tempimage.jpg' --include='*.jpg' --exclude='*' labeled/ /usb/&&rm -rf *.jpg
 done
